c67d324c7ea252d230a188c6b73ae9700753a464
Nathan Elg <elgn@students.wwwu.edu>
2017-06-14 15:32:35 -0600

    added schema comments

--- server/models/household.js
+++ server/models/household.js
@@ -7,12 +7,14 @@ var schema = new mongoose.Schema({
   totalPoints: { type: Number, default: 0 },
 	finished: { type: Boolean, default: false },
 	choreLog: { type: Array },
+	//choreLog is the chores that have been completed by all the users on a certain household
   created: { type: Number, default: Date.now() },
 	startDate: { type: Number },
   creatorId: {type: ObjectId, ref: models.user.name, required: true},
   // Relations
 	chores: { type: ObjectId, ref: models.chore.name},
 	choresList: {type: Array},
+	//choresList is the chores that are added to the household when it is created
 	members: { type: Array},
 	prize: { type: ObjectId, ref: models.prize.name},
 	leaderboard: {type: ObjectId, ref: models.leaderboard.creatorId }
