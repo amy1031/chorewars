b1b0c92b49e804e66e0ea1b01bf40bf92fc47eaa
Nathan Elg <elgn@students.wwwu.edu>
2017-06-16 15:34:19 -0600

    get rid of bootstrap error

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <nav class="navbar fixed-top navbar-light bg-faded">
       <a class="navbar-brand" href="">
-        <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""> Bootstrap
+        <img src="" width="30" height="30" class="d-inline-block align-top" alt=""> Bootstrap
         <button type="button" class="btn btn-outline-danger" @click="logout">Logout</button>
       </a>
     </nav>
