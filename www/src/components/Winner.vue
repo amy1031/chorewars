6f09d6835c5e85261ece0a8d9c85e244cb9cece6
Amy Roche <amy.roche31@gmail.com>
2017-06-25 18:12:38 -0600

    winner styling

--- www/src/components/Winner.vue
+++ www/src/components/Winner.vue
@@ -1,35 +1,55 @@
 <template>
-  <div class="winner">
-    <h1>{{this.activeHousehold.name}}</h1>
-    <h6>Scoreboard:</h6>
-        <ul>
-            <li v-for='member in scoreBoard'>{{member.name}} - {{member.points}}
-            </li>
-        </ul>
-        <button type="button" class="btn btn-success" @click="resetHousehold">End Household</button>
-  </div>
+    <div class="winner">
+        <navbar></navbar>
+        <div class="row justify-content-sm-center">
+            <div class="name">
+                <div class="col-12 text-center">
+                    <h1 class="text-center">{{activeHousehold.name}}</h1>
+                </div>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3 class="scoreboard">Scoreboard:</h3>
+                <div v-for='member in scoreBoard'>
+                    <span class="membername">{{member.name}}</span>
+                    <span class="memberpoints">{{member.points}}</span>
+                </div>
+                <h6 v-if="scoreBoard == {}" class="text-center">No one in this household has earned any points yet.</h6>
+            </div>
+        </div>
+        <div class="row justify-content-sm-center">
+            <div class="end">
+                <div class="col-12 text-center">
+                    <button type="button" class="btn btn-success resethh" @click="resetHousehold">End Household</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
-  name: 'winner',
-  data(){
-    return {
+    name: 'winner',
+    data() {
+        return {
 
-    }
-  },
-  mounted() {
-      this.$store.dispatch('getHouseholds')
-  },
-  computed:{
-    user(){
-      return this.$store.state.user
+        }
     },
-    activeHousehold() {
-      return this.$store.state.activeHousehold
+    mounted() {
+        this.$store.dispatch('getHouseholds')
     },
-    scoreBoard() {
+    computed: {
+        user() {
+            return this.$store.state.user
+        },
+        activeHousehold() {
+            return this.$store.state.activeHousehold
+        },
+        scoreBoard() {
             let pointsDictionary = {}
             for (var i = 0; i < this.activeHousehold.members.length; i++) {
 
@@ -49,17 +69,56 @@ export default {
             }
             return pointsDictionary
         }
-  },
-  methods:{
-    resetHousehold() {
-          this.$store.dispatch("resetHousehold", this.activeHousehold)
+    },
+    methods: {
+        resetHousehold() {
+            this.$store.dispatch("resetHousehold", this.activeHousehold)
         }
-  },
-  components:{}
+    },
+    components: {
+        Navbar
+    }
 }
 </script>
 
 
 <style scoped>
+.name {
+    padding-top: 10px;
+    text-align: center;
+}
 
+.membername {
+    font-size: 25px;
+    padding-left: 20px;
+    line-height: 35px;
+    padding-bottom: 5px;
+}
+
+.memberpoints {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    font-size: 20px;
+    color: #000;
+    line-height: 20px;
+    text-align: center;
+    background: #5cb85c;
+    padding: 10px;
+}
+
+button.resethh {
+    font-size: 18px;
+    font-weight: bold;
+    font-family: helvetica;
+    border-radius: 5px;
+    border: 0px;
+    padding: 10px;
+    margin-bottom: 5px;
+    transition-duration: 0.4s;
+}
+
+.end {
+  padding-top: 20px;
+}
 </style>
